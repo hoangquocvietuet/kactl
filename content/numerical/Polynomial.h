56329eff4d8f1a0a4518a94730f6d0b040eb7d1c
Vo Khac Trieu <72329092+DeMen100ns@users.noreply.github.com>
2023-11-30 17:16:00 +0700

    Update Polynomial.h

--- content/numerical/Polynomial.h
+++ content/numerical/Polynomial.h
@@ -3,61 +3,6 @@
  * Date: 2023-11-30
  * Description:
  */
-#pragma once
-
-template <int32_t MOD>
-struct modint {
-  int32_t value;
-  modint() = default;
-  modint(int32_t value_) : value(value_) {}
-  inline modint<MOD> operator + (modint<MOD> other) const { int32_t c = this->value + other.value; return modint<MOD>(c >= MOD ? c - MOD : c); }
-  inline modint<MOD> operator - (modint<MOD> other) const { int32_t c = this->value - other.value; return modint<MOD>(c <    0 ? c + MOD : c); }
-  inline modint<MOD> operator * (modint<MOD> other) const { int32_t c = (int64_t)this->value * other.value % MOD; return modint<MOD>(c < 0 ? c + MOD : c); }
-  inline modint<MOD> & operator += (modint<MOD> other) { this->value += other.value; if (this->value >= MOD) this->value -= MOD; return *this; }
-  inline modint<MOD> & operator -= (modint<MOD> other) { this->value -= other.value; if (this->value <    0) this->value += MOD; return *this; }
-  inline modint<MOD> & operator *= (modint<MOD> other) { this->value = (int64_t)this->value * other.value % MOD; if (this->value < 0) this->value += MOD; return *this; }
-  inline modint<MOD> operator - () const { return modint<MOD>(this->value ? MOD - this->value : 0); }
-  modint<MOD> pow(uint64_t k) const {
-    modint<MOD> x = *this, y = 1;
-    for (; k; k >>= 1) {
-      if (k & 1) y *= x;
-      x *= x;
-    }
-    return y;
-  }
-  modint sqrt() const {
-    if (value == 0) return 0;
-    if (MOD == 2) return 1;
-    if (pow((MOD - 1) >> 1) == MOD - 1)  return 0; // does not exist, it should be -1, but kept as 0 for this program
-    unsigned int Q = MOD - 1, M = 0, i;
-    modint zQ; while (!(Q & 1)) Q >>= 1, M++;
-    for (int z = 1;; z++) {
-      if (modint(z).pow((MOD - 1) >> 1) == MOD - 1) {
-        zQ = modint(z).pow(Q); break;
-      }
-    }
-    modint t = pow(Q), R = pow((Q + 1) >> 1), r;
-    while (true) {
-      if (t == 1) { r = R; break; }
-      for (i = 1; modint(t).pow(1 << i) != 1; i++);
-      modint b = modint(zQ).pow(1 << (M - 1 - i));
-      M = i, zQ = b * b, t = t * zQ, R = R * b;
-    }
-    return min(r, - r + MOD);
-  }
-  modint<MOD> inv() const { return pow(MOD - 2); }  // MOD must be a prime
-  inline modint<MOD> operator /  (modint<MOD> other) const { return *this *  other.inv(); }
-  inline modint<MOD> operator /= (modint<MOD> other)       { return *this *= other.inv(); }
-  inline bool operator == (modint<MOD> other) const { return value == other.value; }
-  inline bool operator != (modint<MOD> other) const { return value != other.value; }
-  inline bool operator < (modint<MOD> other) const { return value < other.value; }
-  inline bool operator > (modint<MOD> other) const { return value > other.value; }
-};
-template <int32_t MOD> modint<MOD> operator * (int64_t value, modint<MOD> n) { return modint<MOD>(value) * n; }
-template <int32_t MOD> modint<MOD> operator * (int32_t value, modint<MOD> n) { return modint<MOD>(value % MOD) * n; }
-template <int32_t MOD> ostream & operator << (ostream & out, modint<MOD> n) { return out << n.value; }
-
-using mint = modint<mod>;
 struct poly {
   vector<mint> a;
   inline void normalize() {
